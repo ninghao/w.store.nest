6fb390b91754d9029384f0d9487b54102bbd215a
wanghao <117663444@qq.com>
2019-11-29 13:33:15 +0800

    定义与使用调整图像尺寸的功能

--- src/modules/avatar/avatar.service.ts
+++ src/modules/avatar/avatar.service.ts
@@ -4,15 +4,18 @@ import { User } from '../user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Avatar } from './avatar.entity';
 import { Repository } from 'typeorm';
+import { ImageProcessService } from '../image-process/image-process.service';
 
 @Injectable()
 export class AvatarService {
   constructor(
     @InjectRepository(Avatar)
     private readonly avatarRepository: Repository<Avatar>,
+    private readonly imageProcess: ImageProcessService,
   ) {}
 
   async store(data: FileDto, user: User) {
+    this.imageProcess.resizeAvatar('uploads/avatar', data.filename);
     return await this.avatarRepository.save({ ...data, user });
   }
 
--- src/modules/image-process/image-process.service.ts
+++ src/modules/image-process/image-process.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import Jimp from 'jimp';
 
 @Injectable()
-export class ImageProcessService {}
+export class ImageProcessService {
+  resizeAvatar(path: string, filename: string) {
+    const filePath = `${path}/${filename}`;
+
+    Jimp.read(filePath).then(image => {
+      image.resize(256, 256).write(`${path}/${filename}-256`);
+      image.resize(128, 128).write(`${path}/${filename}-128`);
+      image.resize(64, 64).write(`${path}/${filename}-64`);
+    });
+  }
+}
